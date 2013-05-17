9ff061c90f98f3648c67a5e1f09e80ec2f2172b6
Matt Evans <matt@thebetacollective.co.uk>
2013-05-17 16:57:19 +0100

    Created relationship with user and groups

--- lib/group.rb
+++ lib/group.rb
@@ -3,4 +3,8 @@ class Group
 	field :group_name, type: String
 	field :url, type: String
 	has_many :posts
+
+  has_and_belongs_to_many :users
+
+# this might not be right- what is the right syntax for the above?
 end
